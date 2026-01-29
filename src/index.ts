fa4cc2b5af6475e36ce28431c2142db76fdff26b
sagar-mahurkar <sagar.0.mahurkar@gmail.com>
2026-01-30 00:03:47 +0530

    Updated index.ts - Added middleware function (dummy authorization)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
-import express, {Request, Response} from "express";
+import express, {Request, Response, NextFunction} from "express";
 
 const app = express();
 
 const PORT = 8080;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello from express and typescript app");
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//   const timestamp = new Date().toISOString();
+//   console.log(`[${timestamp}] ${req.method} ${req.url}`)
+// })
+
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//   (req as any).requestedAt = new Date().toLocaleString();
+//   next();
+// })
+
+const authenticate = (req: Request, res: Response, next: NextFunction) => {
+  const secret = req.query.secret;
+  if (secret === "123") {
+    next();   // allow
+  } else {
+    res.status(401).json({error: "Unauthorized access"});
+  }
+};
+
+app.get("/", authenticate, (req: Request, res: Response) => {
+  res.send("You have accessed a protected route!!");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  res.send("Hello from express and typescript app!!");
 });
 
 app.get("/about", (req: Request, res: Response) => {
