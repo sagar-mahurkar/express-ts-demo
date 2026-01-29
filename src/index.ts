341602c7886d5b160da2cc96074ae4d809ae75c0
sagar-mahurkar <sagar.0.mahurkar@gmail.com>
2026-01-29 23:37:34 +0530

    Updated index.ts - Added about page route

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
-import express from "express";
+import express, {Request, Response} from "express";
 
 const app = express();
 
 const PORT = 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello from express and typescript");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from express and typescript app");
 });
+
+app.get("/about", (req: Request, res: Response) => {
+  res.send("This is about page")
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`)
