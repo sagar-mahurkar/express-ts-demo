e8e9b75332c994ef917cc6843f64ca06d176ee3e
sagar-mahurkar <sagar.0.mahurkar@gmail.com>
2026-01-30 00:36:05 +0530

    Updated index.ts - Added Error handling middleware

--- src/index.ts
+++ src/index.ts
@@ -14,28 +14,35 @@ const PORT = 8080;
 //   next();
 // })
 
-const authenticate = (req: Request, res: Response, next: NextFunction) => {
-  const secret = req.query.secret;
-  if (secret === "123") {
-    next();   // allow
-  } else {
-    res.status(401).json({error: "Unauthorized access"});
-  }
-};
+// const authenticate = (req: Request, res: Response, next: NextFunction) => {
+//   const secret = req.query.secret;
+//   if (secret === "123") {
+//     next();   // allow
+//   } else {
+//     res.status(401).json({error: "Unauthorized access"});
+//   }
+// };
 
-app.get("/", authenticate, (req: Request, res: Response) => {
-  res.send("You have accessed a protected route!!");
+// app.get("/", authenticate, (req: Request, res: Response) => {
+//   res.send("You have accessed a protected route!!");
+// });
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error("Something went wrong");
+  next(err);
 });
 
 app.get("/data", (req: Request, res: Response) => {
   res.send("Recieved a GET request");
-})
+});
+
 app.post("/data", (req: Request, res: Response) => {
   res.send("Recieved a POST request");
-})
+});
+
 app.delete("/data", (req: Request, res: Response) => {
   res.send("Recieved a DELETE request")
-})
+});
 
 app.post("/hello", (req: Request, res: Response) => {
   res.send("Hello from express and typescript app!!");
@@ -43,7 +50,15 @@ app.post("/hello", (req: Request, res: Response) => {
 
 app.get("/about", (req: Request, res: Response) => {
   res.send("This is about page")
-})
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error("ERROR:", err.message);
+  res.status(500).json({
+    status: "error",
+    message: err.message
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`)
